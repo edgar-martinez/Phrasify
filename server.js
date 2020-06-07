0d2d828ccce8da1d1359a613bb1e5cbadbe7da3f
Edgar Martinez <emartinez@Edgars-MacBook-Pro.local>
2020-06-07 16:01:23 -0500

    _

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ let app   = require("quik-server")
 app.quikAdd("quik-backend")         // this does the backend/api magic
 app.quikAdd("quik-dom")             // this does the JSX magic
 
+let port = process.env.PORT || 3030
 app.settings = {
     // default settings (all are optional)
-    port: process.env.PORT || 3030,
+    port: port,
     host: '0.0.0.0',
     websiteFile: "./website.jsx",
     codeFolder: "./code",
     bundlerOptions: {}, // see https://parceljs.org/api.html for options
     afterServerStarted: () => {
-        console.log(`Server running on http://localhost:3030`)
+        console.log(`Server running on http://localhost:${port}`)
     }
 }
 app.start()
